dee01b53defd3d10a90b3d5133f49738ef77d05b
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-09-07 04:59:43 +0800

    Add #add_item method

--- lib/pivot/person.rb
+++ lib/pivot/person.rb
@@ -1,5 +1,20 @@
 module Pivot
   class Person
-    # Code here!
+
+    attr_accessor :email, :first_name, :last_name, :items 
+
+    def initialize (email:, first_name:, last_name:)
+      self.email = email
+      self.first_name = first_name
+      self.last_name = last_name
+    end
+
+    def add_item(item, &block)
+      item.assignee = self.email  
+      self.items = Array.new
+      self.items << item
+      block.call if block_given?
+    end
+
   end
 end
