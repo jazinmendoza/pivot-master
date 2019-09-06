7af1e9dbf2da25730daadf8ad00e7de5a25729c0
Jazin Mendoza <jazinmendoza16@gmail.com>
2019-09-07 04:57:51 +0800

    Add #valid, #project_code and #+ methods

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,26 @@
 module Pivot
   class Item
-    # Code here!
+    include Enumerable
+
+    attr_accessor :name, :assignee, :points
+
+    def initialize (name:, assignee: nil, points:)
+      self.name = name
+      self.assignee = assignee
+      self.points = points
+    end
+
+    def project_code
+      project_code = @name.split("-")
+      return project_code[0]
+    end
+
+    def valid?
+      return project_code == "EREC"||project_code == "AZR"
+    end
+
+    def +(other)
+      return self.points + other.points
+    end
   end
 end
